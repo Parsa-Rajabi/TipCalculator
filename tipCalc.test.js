9ec4d323f21a80e9e3c76ee7c25b23ad984e27b3
Parsa <ParsaRajabiPR@gmail.com>
2020-09-15 19:00:22 -0700

    added Jest testing for functions in tipCalc.js

---
+++ tipCalc.test.js
@@ -0,0 +1,18 @@
+const tripCalc = require('./tipCalc');
+// const addNumbers = require('./tipCalc'o);
+
+test('Submit button exits', () => {
+    expect(document.getElementsByClassName('btn btn-default')).toBeTruthy();
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(tripCalc.addNumbers(1, 2)).toBe(3);
+});
+
+test('divides 4 by 2 to equal 2', () => {
+    expect(tripCalc.divideNumbers(4, 2)).toBe(2);
+});
+
+test('multiply 2 by 3 to equal 6', () => {
+    expect(tripCalc.multiplyNumbers(3, 2)).toBe(6);
+});
